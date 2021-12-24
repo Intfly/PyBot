9e6f1ef6712c25b6a2b46933e168d4347fd7eedc
Intfly <71024388+Intfly@users.noreply.github.com>
2021-12-24 20:56:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ can.place(x=-2,y=-2)
 olhelper = 0
 next_helper = 0
 home_hlp = 0
+apos='"'
 
 
 def menu_home():
@@ -39,6 +40,10 @@ def menu_home():
 def interface_suivante():
     if next_helper == 1:
         tuto_2()
+    elif next_helper == 2:
+        tuto_3()
+    elif next_helper == 3:
+        tuto_4()
 
 #premier menu en cliquant sur "tutoriel" 
 def tuto_1():
@@ -62,6 +67,8 @@ def tuto_1():
     button_retour_home.place(x=25,y=450)
 
 def tuto_2():
+    global next_helper
+    next_helper = 2
     can.delete(ALL)
     for widgets in frame_p.winfo_children():
         if widgets.winfo_class() != 'Canvas':
@@ -69,11 +76,38 @@ def tuto_2():
     can.create_image(0,0,image=linear_gradient_1)
     can.create_image(10,10,image=img_bck_white)
     can.create_text(150,50,fill="#7256B2",text="prérequis",font=Font_titre_gros)
-    can.create_text(300,165,fill="white",text="créez une nouvelle application",font=Font_desc)
+    can.create_text(300,165,fill="white",text="-créez une nouvelle application grâce au bouton en haut à droite",font=Font_desc)
+    can.create_image(120,210, image=new_app)
+    can.create_text(120,260,fill="white",text="vous y trouverez",font=Font_desc)
+    can.create_text(273,260,fill="#ff7583",text="l'ID de l'application",font=Font_desc)
+    can.create_text(443,260,fill="white",text="(vous sera demandé)",font=Font_desc)
+    button_next = Button(frame_p,image = button_next_img_resized,width= 150,height=25,relief=FLAT,bg="#7159b5",fg="#6e63c5",activeforeground="#6e63c5",borderwidth=0,activebackground="#7159b5",highlightbackground="#7159b5",text = 'suivant',compound="center",font=Font_button,command=interface_suivante)
+    button_next.place(x=575,y=450)
     button_retour_home = Button(frame_p,image = button_open_link,width= 120,height=25,relief=FLAT,bg="#6f60c0",fg="white",activeforeground="white",borderwidth=0,activebackground="white",highlightbackground="white",text = 'Menu',compound="center",font=Font_button,command=menu_home)
     button_retour_home.place(x=25,y=450)
 
+def tuto_3():
+    global next_helper
+    next_helper = 3
+    can.delete(ALL)
+    for widgets in frame_p.winfo_children():
+        if widgets.winfo_class() != 'Canvas':
+            widgets.destroy()
+    can.create_image(0,0,image=linear_gradient_1)
+    can.create_image(10,10,image=img_bck_white)
+    can.create_text(150,50,fill="#7256B2",text="prérequis",font=Font_titre_gros)
+    can.create_text(225,175,fill="white",text=f"-dans l'onglet {apos}Bot{apos} du menu à gauche\n\n       ajoutez un nouveau Bot",font=Font_desc)
+    can.create_image(150,250, image=new_bot)
+    can.create_text(180,300,fill="white",text=f"vous y trouverez le ",font=Font_desc)
+    can.create_text(320,300,fill="#ff7583",text=f"TOKEN du bot",font=Font_desc)
+    can.create_text(475,300,fill="white",text="(vous sera demandé)",font=Font_desc)
+    button_next = Button(frame_p,image = button_next_img_resized,width= 150,height=25,relief=FLAT,bg="#7159b5",fg="#6e63c5",activeforeground="#6e63c5",borderwidth=0,activebackground="#7159b5",highlightbackground="#7159b5",text = 'suivant',compound="center",font=Font_button,command=interface_suivante)
+    button_next.place(x=575,y=450)
+    button_retour_home = Button(frame_p,image = button_open_link,width= 120,height=25,relief=FLAT,bg="#6f60c0",fg="white",activeforeground="white",borderwidth=0,activebackground="#6A72D9",highlightbackground="white",text = 'Menu',compound="center",font=Font_button,command=menu_home)
+    button_retour_home.place(x=25,y=450)
 
+def tuto_4():
+    print(1)
 #premier menu en cliquant sur "nouveau" 
 def nouveau_1():
     can.delete(ALL)
